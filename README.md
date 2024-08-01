ff47570b46802382c5212aa7c93c0a00e1a147f5
2kn96 <dorachankan96@icloud.com>
2024-08-01 23:24:02 +0900

    submit

--- README.md
+++ README.md
@@ -10,10 +10,31 @@
 ## Summary
 本ページで紹介する **"Instant Neural Grasphics Primitives with a Multiresolution Hash Encoding"** では、Neural Radiance Fields(NeRF)をはじめとする三次元再構成手法に使われるNeural Graphics Primitives(NGP)の位置エンコーディングに注目したものである。オリジナルなNeRF[2]ではfrequency encodingを使用していたのに対し、ここではhash tableを用いた位置エンコーディングを行っている。これによりパラメータを減らし、学習とレンダリングの効率と精度が向上した。
 
+
 ## Implementation and Test
+### About Multiresolution Hash Encoding 
+ここではNeRFへの利用を考え3次元座標に対するmultiresolution hash encodingを実装した。以下の手順に基づいている。
+1. 解像度のレベルの数$L$とハッシュテーブルのサイズ$T$、特徴ベクトルの次元$F$を定める。また、空間の分割数の上限と下限$(N_{min}\leq N_l\leq N_{max},0\leq l\leq L)$を定める。ただし$N_l$は$$N_l=\lfloor N_{min}b^l\rfloor,b=exp((\ln N_{max}-\ln N_{min})/(L-1))$$で決定される。
+1. 入力される位置座標$\boldsymbol{x}$に対し、これが含まれるグリッドの頂点座標をハッシュ関数$$h(x) = \left( \bigoplus_{i=1}^{d} x_i \pi_i \right) \mod T$$によって$F$次元の特徴ベクトルに変換される。ここで$\pi_i$は論文で使われている$[1,2654435761,805459861]$という大きな素数を用いている。なお、この特徴ベクトルは後続のネットワークと一緒に学習される。
+1. 各頂点の特徴ベクトルが線形補完されることで最終的に入力座標に対応する特徴ベクトルを得る。
 
+この位置エンコーダによって各レベルで$F$次元の特徴ベクトルに変換されるから、各座標を$L\cdot F$次元の特徴量で表すことになる。
+
+### About Implementation and Test
+3次元のmultiresolution hash encoderを実装し、これを使ってある関数$f(x,y,z)$を学習タスク実行した。なお、hash encoderの実装には[2]を参考にした。\
+ハイパーパラメータは、$$L=16,T=2^8,F=3,N_{min}=4,N_{max}=64$$を使用した。\
+学習する領域は$120\times 120\times 120$の立方体領域で、$$f(x,y,z)=(x-x_0)^2+(y-y_0)^2+(z-z_0)^2-r^2$$ $$(x_0,y_0,z_0)=(60,60,60),r=60$$を学習した。
+学習率等これ以外のハイパーパラメータは論文中で使われているものを使用した。
+### Result
+学習の結果$f(x,y,z)=0$の曲面が半径60の球面になっていればよい。今回は解像度が低い空間を使用していることも考慮し、$f^2<5$となる格子点を$f(x,y,z)=0$としてプロットした。
+![result1](result1.png "Resut")
+隙間が空いているところもあり完ぺきではないものの、教師データとして与えた球面をよく表現しているといえる。
 ## Key Points on this paper
-
+NeRFが発表された当初も三次元再構成の手法として革新的であったことに違いはないが、学習時間が多くかかってしまうことが問題だった。しかしこの論文のmultiresolution hash talbeを使うことで無駄な解像度部分をスキップするような働きがあり位置的な特徴を効率的にとらえ、論文内では数秒でNeRFの学習が可能になったとしている。\
+さらに従来の周波数エンコーディングではベクトルの次元を増やすことでしか高周波成分の学習ができなかったため、精度を高めるためには学習速度を犠牲にする必要があった。逆もまたしかりである。いっぽうでmultiresolution hash encodingでは特徴ベクトルも最適化することで速度と精度をある程度両立できる利点がある。また、各レベルの仕様と特徴ベクトルの初期値を決めるだけでよいことからハイパーパラメータチューニングも簡単になることもこの論文の大きな貢献であるといえる。
 ## References
-[1]\
-[2]
+[1]Thomas Müller, et al.,"Instant Neural Graphics Primitives with a Multiresolution Hash Encoding",ACM Transaction on Graphics, Volume 41, Issue 4,No.:102, Pages 1-15
+https://dl.acm.org/doi/10.1145/3528223.3530127\
+[2]Ben Mildenhall, et al.,"NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis",arXiv:2003.08934\
+[3]株式会社オプティム"InstantNeRFの心臓、Multiresolution Hash Encodingをシンプルに実装しつつ2次元画像で試してみる"
+https://tech-blog.optim.co.jp/entry/2022/04/22/100000#f-0ec67ba2
--- hashencoder.py
+++ hashencoder.py
@@ -2,33 +2,132 @@ import math
 import numpy as np
 import torch
 from torch import nn
-from torch.nn import functional as F
+from torch.nn import functional as Fnc
+from torchvision.io import read_image, ImageReadMode
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+#D = 2       #Dimension of input
 #L = 16      #Number of levels
 #T = 2**14   #hash table size
 #F = 2       #Number of feature dimensions per entry
 #N_min = 16  #Coarsest resolution
 #N_max = 512 #Finest resolution
+init_range = 1e-4
 
-def norm_coord(width, height):
-    x, y = torch.meshgrid(torch.linspace(0,1,steps = width),torch.linspace(0,1,steps = height),indexing = 'xy')
-    return torch.stack([x, y])
+def norm_coord(width, height, depth):
+    x, y, z = torch.meshgrid(torch.linspace(0,1,steps = width),torch.linspace(0,1,steps = height),torch.linspace(0,1,steps = depth),indexing = 'xy')
+    return torch.stack([x, y, z])
 
-def HashEncoder(nn.Module):
-    def __init__(self, L=16, T=2**14, F=2, N_min=16, N_max=512, interpolation='bilinear'):
+class HashEncoder(nn.Module):
+    def __init__(self, L=16, T=2**8, F=3, N_min=4, N_max=64):
         super().__init__()
         self.L = L
         self.T = T
         self.F = F
-        self.interpolation = interpolation
         b = math.exp((math.log(N_max/N_min))/(L-1))
-        self.N_l =  [floor(N_min*(b**l)) for l in range(L)]
-        self.Pi('primes',torch.tensor([1, 2654435761]))
-        self.hash_table = nn.Parameter(torch.rand([1, T, F], requires_grid=True)*2e-3-1e-3)
+        self.N_l = []    #Each numbers of divisions 
+        for l in range(L):
+            self.N_l.append(math.floor(N_min * (b**l)))
+        self.register_buffer('pi_i',torch.tensor([1, 2654435761, 805459861]))  #table of prime
+        self.hash_table = nn.Parameter(torch.rand([L, T, F], requires_grad=True)*2*init_range-init_range)
 
     @property
     def enc_size(self):
         return self.L * self.F
     
     def forward(self, x):
-        
+        b, c, h, w, d= x.size()
+        def make_grid(x, n):
+            grid = Fnc.max_pool3d(x*n, (h//n, w//n, d//n)).to(dtype=torch.long)
+            grid = grid*self.pi_i.view([3, 1, 1, 1])
+            grid = (grid[:,0]^grid[:,1]^grid[:,2])%self.T
+            return grid
+
+        grids = []
+        for n in self.N_l:
+            grids.append(make_grid(x, n))
+        feat = []
+        for i,g in enumerate(grids):
+            feat.append(self.hash_table[i, g].permute(0, 4, 1, 3, 2))
+        feat_vec = []
+        for f in feat:
+            feat_vec.append(Fnc.interpolate(f, (h, w, d), mode='trilinear')) #outputs feature vector
+        feat_map = torch.hstack(feat_vec)
+        return feat_map
+
+class Net(nn.Module):
+    def __init__(self, encoder):
+        super().__init__()
+        input_size = encoder.enc_size
+        self.fc1 = nn.Conv3d(input_size, 16, 1)
+        self.fc2 = nn.Conv3d(16, 1, 1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = Fnc.sigmoid(x)
+        x = self.fc2(x)
+        return x
+
+class MainModel(nn.Module):
+    def __init__(self):
+        super(MainModel, self).__init__()
+        self.net1 = HashEncoder()
+        self.net2 = Net(HashEncoder())
+    
+    def forward(self, x):
+        x = self.net1(x)
+        x = self.net2(x)
+        return x
+
+def ref_func(r):
+    r = r.squeeze(0)
+    x, y, z = r
+    R = 60
+    return ((x-60)**2 + (y-60)**2 + (z-60)**2 - R**2).unsqueeze(0)
+
+def plot_surface(coords, f):
+    # フィルタリングされた点を3Dプロット
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    print(f)
+    for i in range(120):
+        for j in range(120):
+            for k in range(120):
+                if f[0,i,j,k].item()**2<5:
+                    ax.plot(i,j,k,c='b', marker='o')
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.set_xlim=([0, 119])
+    ax.set_ylim=([0, 119])
+    ax.set_zlim=([0, 119])
+    plt.show()
+
+main = MainModel()
+
+vec = norm_coord(120, 120, 120)
+vec = vec.unsqueeze(0)
+y = ref_func(120*vec)
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam([{'params':main.net1.parameters()}, {'params': main.net2.parameters()}], lr=0.1, betas=(0.9, 0.99), eps=1e-15)
+
+num_epoch = 100
+losses = []
+for i in range(num_epoch):
+    main.train()
+    optimizer.zero_grad()
+    y_pred = main(vec).squeeze(0)
+    loss = criterion(y_pred, y)
+    loss.backward()
+    optimizer.step()
+    losses.append([loss.item()])
+
+plot_surface(vec, y_pred)
+plt.plot(range(num_epoch),losses)
+plt.yscale('log')
+plt.title('loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
